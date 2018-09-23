f170c8905d66185ada49c5cb485c56e0372caa7d
Sescheraun <rcollier@madisoncollege.edu>
2018-09-23 12:59:40 -0500

    Starting to work on the date processing.

--- src/main/java/com/sescheraun/DailyGreeter.java
+++ src/main/java/com/sescheraun/DailyGreeter.java
@@ -1,7 +1,22 @@
 package com.sescheraun;
 
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
 import javax.servlet.jsp.tagext.SimpleTagSupport;
+import java.io.IOException;
+import java.time.LocalDate;
+
+import static java.time.LocalDate.now;
 
 public class DailyGreeter extends SimpleTagSupport {
+
+    @Override
+    public void doTag() throws JspException, IOException {
+        LocalDate localDate = now();
+        super.doTag();
+        JspWriter out = getJspContext().getOut();
+        out.println(localDate.getDayOfMonth());
+
+    }
 
 }
